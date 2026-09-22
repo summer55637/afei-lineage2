1bc31b6202b310dbf691de51e937f0ea889313aa
9527 <tttt0977661664@gmail.com>
2026-09-22 16:41:37 +0800

    i18n: translate ranking reward float text

--- lineage-idle/src/services/RankingService.js
+++ lineage-idle/src/services/RankingService.js
@@ -187,7 +187,7 @@ export const 排行榜Service = {
     }
 
     log(`🏆 **[每日排行榜獎勵－排名 #${rank}]** 你獲得 ${coins} 亞丁幣、${adena.toLocaleString()} 金幣${scrolls > 0 ? `、${scrolls}x 強化卷軸` : ''}!`, 'rarity-legendary');
-    floatText(`+${coins} COINS!`, 'float-jackpot');
+    floatText(`+${coins} 亞丁幣！`, 'float-jackpot');
 
     onUpdate();
     return { success: true, rank, coins, adena, scrolls };
