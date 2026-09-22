c7a6d023b0fe9f529421adbf523b7836244048df
9527 <tttt0977661664@gmail.com>
2026-09-23 05:58:07 +0800

    i18n: localize augmentation glow names

--- lineage-idle/src/services/AugmentationService.js
+++ lineage-idle/src/services/AugmentationService.js
@@ -141,7 +141,12 @@ export class AugmentationService {
       .join('、');
 
     const skillText = acquiredSkill ? ` 並獲得稀有技能 [${acquiredSkill.name}]` : '';
-    const glowText = hasGlow ? ` ✨ 獲得光效（${glowColor}）！` : '';
+    const glowLabel = ({
+      'golden-amber': '金色琥珀',
+      'radiant-purple': '璀璨紫光',
+      'arcane-blue': '奧術藍光'
+    })[glowColor] || glowColor;
+    const glowText = hasGlow ? ` ✨ 獲得光效（${glowLabel}）！` : '';
 
     const triumphMsg = `💎 增幅成功！${weapon.name || '你的武器'} 獲得：[${statSummary}]${skillText}${glowText}`;
     log(triumphMsg, 'success');
